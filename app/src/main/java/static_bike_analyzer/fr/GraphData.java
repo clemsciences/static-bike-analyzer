ad818d6c5a4988586044778d4eaf0b4d79ccefaa
clement_besnier <clemsciences@aol.com>
2018-06-26 18:30:27 +0200

    Graph manager soon

---
+++ app/src/main/java/static_bike_analyzer/fr/GraphData.java
@@ -0,0 +1,33 @@
+package static_bike_analyzer.fr;
+
+import org.json.JSONArray;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by clement_besnier on 26/06/2018.
+ */
+
+public class GraphData {
+
+    public GraphData()
+    {
+        JSONArray series = new JSONArray();
+    }
+    public void fromSeries()
+    {
+
+    }
+
+
+    public void addPoint(int curve, int time, float velocity)
+    {
+
+    }
+    public void getCurve()
+
+    {
+
+    }
+}
---
+++ app/src/main/java/static_bike_analyzer/fr/GraphManager.java
@@ -0,0 +1,17 @@
+package static_bike_analyzer.fr;
+
+import org.json.JSONArray;
+
+/**
+ * Created by clement_besnier on 26/06/2018.
+ */
+
+public class GraphManager {
+
+    public JSONArray toJSON()
+    {
+        return new JSONArray();
+
+    }
+
+}
